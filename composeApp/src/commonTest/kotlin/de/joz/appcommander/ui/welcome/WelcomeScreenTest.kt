0c2e5c0ec0dc4b9e3b510e61c96eca281e424d12
Jan Oliver Zieger <ollizieger@gmx.de>
2025-10-26 13:52:05 +0100

    find bug

--- composeApp/src/commonTest/kotlin/de/joz/appcommander/ui/welcome/WelcomeScreenTest.kt
+++ composeApp/src/commonTest/kotlin/de/joz/appcommander/ui/welcome/WelcomeScreenTest.kt
@@ -42,6 +42,10 @@ class WelcomeScreenTest {
 
 	private val preferencesRepositoryMock = PreferencesRepositoryMock()
 
+	private val testClass: Class<T> = this
+	private val storeDirectory: File = File("./build/reports/tests/screenshots/")
+	private val goldenImageDirectory: File = File("./src/commonTest/kotlin/")
+
 	@BeforeTest
 	fun setup() {
 		koin =
@@ -77,7 +81,7 @@ class WelcomeScreenTest {
 			onNodeWithText("Let's go!").assertIsDisplayed().assertHasClickAction()
 			onNodeWithText("Do not show welcome screen again.").assertIsDisplayed()
 
-			screenshotVerifier.verifyScreenshot(source = this, screenshotName = "all_labels")
+			verifyScreenshot(source = this, screenshotName = "all_labels")
 		}
 	}
 
@@ -163,5 +167,155 @@ class WelcomeScreenTest {
 				},
 			)
 		}
+	}
+	fun verifyScreenshot(
+		source: ComposeUiTest,
+		screenshotName: String,
+	) {
+		println("1")
+		val screenshotResult =
+			takeScreenshot(
+				source = source,
+				screenshotName = screenshotName,
+			)
+		println("Result is: $screenshotResult")
+		when (screenshotResult) {
+			is ScreenshotResult.Success,
+				->
+				verifyAgainstGoldenImage(
+					screenshotFile = screenshotResult.screenshot,
+				)
+
+			is ScreenshotResult.Failure -> {
+				screenshotResult.error
+			}
+		}
+	}
+
+	private fun takeScreenshot(
+		source: ComposeUiTest,
+		screenshotName: String,
+	): ScreenshotResult =
+		runCatching {
+			println("2")
+			val image = source.onNode(isRoot()).captureToImage()
+			val bytearray = convertToPng(image.asSkiaBitmap())
+
+			if (bytearray == null || bytearray.isEmpty()) {
+				println("3")
+				throw Exception("Screenshot is empty.")
+			}
+
+			val file = File(storeDirectory, "$screenshotName.png")
+			file.writeBytes(bytearray)
+			println("4")
+			ScreenshotResult.Success(screenshot = file)
+		}.getOrElse { throwable ->
+			ScreenshotResult.Failure(error = throwable)
+		}
+
+	private fun verifyAgainstGoldenImage(screenshotFile: File) {
+		val goldenImage =
+			readGoldenImageFromSrcDir(
+				screenshotFileName = screenshotFile.name,
+			)
+
+		val compareResult =
+			if (goldenImage.exists()) {
+				Arrays.compare(screenshotFile.readBytes(), goldenImage.readBytes())
+			} else {
+				IMAGE_DOES_NOT_EXIST
+			}
+
+		if (compareResult == IMAGE_DOES_NOT_EXIST) {
+			Files.copy(screenshotFile.toPath(), goldenImage.toPath(), StandardCopyOption.REPLACE_EXISTING)
+		} else if (compareResult != IDENTICAL_IMAGES) {
+			makeDiff(goldenImage, screenshotFile)
+			val currentScreenshot = File(goldenImage.parentFile, "${goldenImage.nameWithoutExtension}_current.png")
+			Files.copy(screenshotFile.toPath(), currentScreenshot.toPath(), StandardCopyOption.REPLACE_EXISTING)
+		}
+
+		val failMessage =
+			when (compareResult) {
+				IDENTICAL_IMAGES -> "WILL NOT PRINT IN TEST RESULT."
+				IMAGE_DOES_NOT_EXIST -> "Fail: Golden image does not exist. Copied screenshot for you:)."
+				else -> "Fail: Screenshots are not identical. Created screenshot diff to directory."
+			}
+
+		assertEquals(
+			IDENTICAL_IMAGES,
+			compareResult,
+			"$failMessage\n" +
+				"Current: ${screenshotFile.absolutePath}\n" +
+				"Golden: ${goldenImage.absolutePath}\n" +
+				"Verify your screenshots in your VCS.",
+		)
+	}
+
+	private fun makeDiff(
+		goldenImage: File,
+		screenshotFile: File,
+	) {
+		val image1Bitmap = createBitmapFromScreenshot(screenshotFile = goldenImage)
+		val image2Bitmap = createBitmapFromScreenshot(screenshotFile = screenshotFile)
+
+		if (image1Bitmap.width != image2Bitmap.width || image1Bitmap.height != image2Bitmap.height) {
+			throw IllegalStateException("Images are not the same size.")
+		}
+
+		val destination = Bitmap()
+		destination.allocPixels(image1Bitmap.imageInfo)
+		val canvas = Canvas(destination)
+		val paint =
+			Paint().apply {
+				color = 0xFFFF0000.toInt()
+			}
+		val normalPaint = Paint()
+		for (x in 0 until image1Bitmap.width) {
+			for (y in 0 until image1Bitmap.height) {
+				val pixelColor1 = image1Bitmap.getColor(x = x, y = y)
+				val pixelColor2 = image2Bitmap.getColor(x = x, y = y)
+				canvas.drawPoint(
+					x.toFloat(),
+					y.toFloat(),
+					if (pixelColor1 == pixelColor2) normalPaint.apply { color = pixelColor1 } else paint,
+				)
+			}
+		}
+
+		val diffFile = File(goldenImage.parentFile, "${goldenImage.nameWithoutExtension}_diff.png")
+		diffFile.writeBytes(convertToPng(destination)!!)
+	}
+
+	private fun readGoldenImageFromSrcDir(screenshotFileName: String): File {
+		val sourceDirectory =
+			testClass.name
+				.split(".") // split class name into parts
+				.dropLast(1) // remove class name
+				.joinToString("/") // convert to directory path
+
+		val parentScreenshotDir = File(goldenImageDirectory.absolutePath.plus("/$sourceDirectory/screenshots/"))
+		parentScreenshotDir.mkdir()
+
+		return File(parentScreenshotDir, screenshotFileName)
+	}
+
+	private fun createBitmapFromScreenshot(screenshotFile: File): Bitmap {
+		val imageBitmap = Bitmap()
+		val image = Image.makeFromEncoded(screenshotFile.readBytes())
+
+		imageBitmap.allocPixels(image.imageInfo)
+		image.readPixels(imageBitmap)
+
+		return imageBitmap
+	}
+
+	private fun convertToPng(bitmap: Bitmap): ByteArray? =
+		Image.makeFromBitmap(bitmap).encodeToData(EncodedImageFormat.PNG, IMAGE_QUALITY)?.bytes
+
+	companion object Companion {
+		private const val IMAGE_QUALITY = 100
+		private const val IDENTICAL_IMAGES = 0
+		private const val IMAGE_DOES_NOT_EXIST = -1
 	}
 }
