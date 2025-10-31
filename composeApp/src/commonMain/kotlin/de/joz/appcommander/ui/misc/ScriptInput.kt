21aee5c1263b1fc3daa6b119f79d2d722081c666
Jan Oliver Zieger <ollizieger@gmx.de>
2025-10-31 13:48:45 +0100

    Fix/Improve

--- composeApp/src/commonMain/kotlin/de/joz/appcommander/ui/misc/ScriptInput.kt
+++ composeApp/src/commonMain/kotlin/de/joz/appcommander/ui/misc/ScriptInput.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
@@ -17,6 +16,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.text.TextStyle
 import compose.icons.FeatherIcons
 import compose.icons.feathericons.Play
 import de.joz.appcommander.ui.theme.AppCommanderTheme
@@ -40,8 +40,8 @@ fun ScriptInput(
 				unfocusedIndicatorColor = Color.Transparent,
 			),
 		textStyle =
-			LocalTextStyle.current.copy(
-				color = MaterialTheme.colorScheme.background,
+			TextStyle.Default.copy(
+				color = Color.Black,
 			),
 		onValueChange = {
 			inputValue = it
