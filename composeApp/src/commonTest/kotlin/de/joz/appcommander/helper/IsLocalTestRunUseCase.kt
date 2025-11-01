fcca6f82123aeaba91de0b45e31e16910fea96e8
Jan Oliver Zieger <ollizieger@gmx.de>
2025-11-01 16:19:14 +0100

    Fix/Improve

--- composeApp/src/commonTest/kotlin/de/joz/appcommander/helper/IsLocalTestRunUseCase.kt
+++ composeApp/src/commonTest/kotlin/de/joz/appcommander/helper/IsLocalTestRunUseCase.kt
@@ -2,15 +2,8 @@ package de.joz.appcommander.helper
 
 class IsLocalTestRunUseCase {
 	operator fun invoke(): Boolean {
-		val systemEnv = System.getenv()
-		if (systemEnv == null || systemEnv.isEmpty()) {
-			return false
-		}
-
-		println("### env: $systemEnv")
-
 		val isDebuggerEnabled = System.getenv("DEBUGGER_ENABLED")
-		val cfBundleIdentifier = System.getenv("__CFBundleIdentifier")
+		val cfBundleIdentifier = System.getenv("__CFBundleIdentifier") ?: ""
 
 		return isDebuggerEnabled.toBoolean() || cfBundleIdentifier.contains("com.google.android.studio")
 	}
