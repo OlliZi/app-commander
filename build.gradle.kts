e0d6aa4bde451bff9571d611f84c7e75846733a8
Jan Oliver Zieger <ollizieger@gmx.de>
2025-12-14 11:28:18 +0100

    remote obsolete plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,6 @@ buildscript {
 }
 
 plugins {
-	alias(libs.plugins.composeHotReload) apply false
 	alias(libs.plugins.composeMultiplatform) apply false
 	alias(libs.plugins.composeCompiler) apply false
 	alias(libs.plugins.kotlinMultiplatform) apply false
