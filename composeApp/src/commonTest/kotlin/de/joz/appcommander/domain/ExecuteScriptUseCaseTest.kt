9a2b1e458a3fd6350ee96b107a1e91d8802f6437
Jan Oliver Zieger <ollizieger@gmx.de>
2026-02-27 16:25:46 +0100

    run chained scripts

--- composeApp/src/commonTest/kotlin/de/joz/appcommander/domain/ExecuteScriptUseCaseTest.kt
+++ composeApp/src/commonTest/kotlin/de/joz/appcommander/domain/ExecuteScriptUseCaseTest.kt
@@ -1,16 +1,19 @@
 package de.joz.appcommander.domain
 
 import de.joz.appcommander.domain.logging.AddLoggingUseCase
+import de.joz.appcommander.helper.IsJenkinsTestRunUseCase
+import de.joz.appcommander.helper.IsLocalTestRunUseCase
 import io.mockk.mockk
 import io.mockk.verify
 import kotlinx.coroutines.test.runTest
-import kotlin.test.Ignore
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 
 class ExecuteScriptUseCaseTest {
 	private val addLoggingUseCaseMock: AddLoggingUseCase = mockk(relaxed = true)
+	private val isLocalTestRunUseCase = IsLocalTestRunUseCase()
+	private val isJenkinsTestRunUseCase = IsJenkinsTestRunUseCase()
 
 	@Test
 	fun `should execute script when launched`() =
@@ -29,8 +32,32 @@ class ExecuteScriptUseCaseTest {
 			val result = executeScriptUseCase(script = script, selectedDevice = "Pixel7")
 
 			assertTrue(result is ExecuteScriptUseCase.Result.Success)
-			assertEquals("1. foo\n", result.output)
+			assertEquals("- foo\n", result.output)
 			verify { addLoggingUseCaseMock.invoke("Execute script: 'echo foo' on device 'Pixel7'.") }
+		}
+
+	@Test
+	fun `should chain multiples scripts when scripts are chained by special command &&`() =
+		runTest {
+			val executeScriptUseCase =
+				ExecuteScriptUseCase(
+					addLoggingUseCase = addLoggingUseCaseMock,
+				)
+			val script =
+				ScriptsRepository.Script(
+					label = "Test",
+					script = "echo foo && echo bar",
+					platform = ScriptsRepository.Platform.ANDROID,
+				)
+
+			val result = executeScriptUseCase(script = script, selectedDevice = "Pixel7")
+
+			assertTrue(result is ExecuteScriptUseCase.Result.Success)
+			assertEquals("- foo\n- bar\n", result.output)
+			verify {
+				addLoggingUseCaseMock.invoke("Execute script: 'echo foo' on device 'Pixel7'.")
+				addLoggingUseCaseMock.invoke("Execute script: 'echo bar' on device 'Pixel7'.")
+			}
 		}
 
 	@Test
@@ -50,7 +77,7 @@ class ExecuteScriptUseCaseTest {
 			val result = executeScriptUseCase(script = script, selectedDevice = "Pixel7")
 
 			assertTrue(result is ExecuteScriptUseCase.Result.Success)
-			assertEquals("1. foo\n,2. foo\n,3. foo\n", result.output)
+			assertEquals("- foo\n- foo\n- foo\n", result.output)
 			verify { addLoggingUseCaseMock.invoke("Execute script: '#LOOP_3 echo foo' on device 'Pixel7'.") }
 		}
 
@@ -72,20 +99,24 @@ class ExecuteScriptUseCaseTest {
 
 			assertTrue(result is ExecuteScriptUseCase.Result.Error)
 			assertEquals(
-				"Cannot run program \"foo_bar_unknown_command\" (in directory \".\"): error=2, No such file or directory",
+				"Cannot run program \"foo_bar_unknown_command\" (in directory \".\"): Exec failed, error: 2 (No such file or directory) ",
 				result.message,
 			)
 			verify {
 				addLoggingUseCaseMock.invoke(
-					"Cannot run program \"foo_bar_unknown_command\" (in directory \".\"): error=2, No such file or directory",
+					"Cannot run program \"foo_bar_unknown_command\" (in directory \".\"): Exec failed, error: 2 (No such file or directory) ",
 				)
 			}
 		}
 
 	@Test
-	@Ignore("cannot executed in CI -> Fix")
 	fun `should append device id in script execution`() =
 		runTest {
+			if (!isLocalTestRunUseCase() && !isJenkinsTestRunUseCase()) {
+				println("Cannot run test on github.")
+				return@runTest
+			}
+
 			val executeScriptUseCase =
 				ExecuteScriptUseCase(
 					addLoggingUseCase = addLoggingUseCaseMock,
@@ -101,9 +132,10 @@ class ExecuteScriptUseCaseTest {
 			val result = executeScriptUseCase(script = script, selectedDevice = "Pixel7")
 
 			assertTrue(result is ExecuteScriptUseCase.Result.Success)
-			assertEquals(
-				listOf("adb", "-s", "Pixel7", "devices"),
-				result.commands,
-			)
+			verify {
+				addLoggingUseCaseMock.invoke(
+					"Execute script: 'adb -s Pixel7 devices' on device 'Pixel7'.",
+				)
+			}
 		}
 }
