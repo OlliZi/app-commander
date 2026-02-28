1bd986c8f0b787aab7f20734675dc367736a65fa
Jan Oliver Zieger <ollizieger@gmx.de>
2026-02-28 15:55:57 +0100

    Fix/Improve

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,3 +25,12 @@ allprojects {
 	apply(plugin = "com.github.ben-manes.versions")
 	apply(plugin = "org.jlleitschuh.gradle.ktlint")
 }
+
+tasks.register<Exec>("convertMov2Gif") {
+	group = "_joz"
+	commandLine(
+		"sh",
+		"-c",
+		"ffmpeg -i preview_overview.mov -pix_fmt rgb8 -r 10 preview_overview.gif && gifsicle -O3 preview_overview.gif -o preview_overview.gif",
+	)
+}
