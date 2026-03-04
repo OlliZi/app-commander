2e21b6134cf4f42ca5d55baca5a5c20f505f2457
Jan Oliver Zieger <ollizieger@gmx.de>
2026-03-04 20:18:22 +0100

    Fix/Improve

--- composeApp/src/commonTest/kotlin/de/joz/appcommander/domain/logging/GetLoggingUseCaseTest.kt
+++ composeApp/src/commonTest/kotlin/de/joz/appcommander/domain/logging/GetLoggingUseCaseTest.kt
@@ -1,23 +1,25 @@
 package de.joz.appcommander.domain.logging
 
+import io.mockk.every
 import io.mockk.mockk
-import io.mockk.verify
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.test.runTest
 import kotlin.test.Test
+import kotlin.test.assertTrue
 
 class GetLoggingUseCaseTest {
 	@Test
 	fun `should get logs from repository`() =
 		runTest {
 			val loggingRepositoryMock: LoggingRepository = mockk(relaxed = true)
+			every { loggingRepositoryMock.logging } returns flowOf(listOf("foo", "bar"))
 
 			val useCase =
 				GetLoggingUseCase(
 					loggingRepository = loggingRepositoryMock,
 				)
 
-			useCase().collect {}
-
-			verify { loggingRepositoryMock.logging }
+			assertTrue(useCase().first().isNotEmpty())
 		}
 }
