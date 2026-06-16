7742f376730c4946534caac5f9b07b5864a3dc7e
Jan Oliver Zieger <ollizieger@gmx.de>
2026-06-16 21:27:41 +0200

    simplify setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,5 @@
 buildscript {
-	repositories {
-		google()
-		mavenCentral()
-		gradlePluginPortal()
-	}
-
 	dependencies {
-		// https://github.com/ben-manes/gradle-versions-plugin
 		classpath(libs.gradle.versions.plugin)
 		classpath(libs.ktlint.gradle)
 	}
@@ -19,9 +12,4 @@ plugins {
 	alias(libs.plugins.ksp) apply false
 	alias(libs.plugins.io.gitlab.arturbosch.detekt)
 	alias(libs.plugins.ktlint)
-}
-
-allprojects {
-	apply(plugin = "com.github.ben-manes.versions")
-	apply(plugin = "org.jlleitschuh.gradle.ktlint")
 }
