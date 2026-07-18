a63299afd3bfe23cdffa1d082ecb47dfb78bcb13
Jan Oliver Zieger <ollizieger@gmx.de>
2026-07-18 09:20:53 +0200

    fix UC

--- sharedCode/src/commonMain/kotlin/de/joz/appcommander/domain/devices/ObserveDevicesUseCase.kt
+++ sharedCode/src/commonMain/kotlin/de/joz/appcommander/domain/devices/ObserveDevicesUseCase.kt
@@ -3,8 +3,9 @@ package de.joz.appcommander.domain.devices
 import de.joz.appcommander.domain.model.Device
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.distinctUntilChanged
-import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.onStart
 import org.koin.core.annotation.Single
 import kotlin.time.Duration.Companion.milliseconds
 
@@ -12,8 +13,8 @@ import kotlin.time.Duration.Companion.milliseconds
 class ObserveDevicesUseCase(
 	private val getDevicesUseCase: GetDevicesUseCase,
 ) {
-	operator fun invoke(): Flow<List<Device>> =
-		flow {
+	private val flowOfDevices = MutableSharedFlow<List<Device>>()
+		.onStart {
 			runCatching {
 				while (true) {
 					println("get devices...")
@@ -24,6 +25,8 @@ class ObserveDevicesUseCase(
 				println(it.message)
 			}
 		}.distinctUntilChanged()
+
+	operator fun invoke(): Flow<List<Device>> = flowOfDevices
 
 	companion object {
 		private val WAIT_DELAY = 3000.milliseconds
