56dcd5c7364feed49055422a1c9614d8fcc8a1e7
Jan Oliver Zieger <ollizieger@gmx.de>
2026-07-23 12:16:39 +0200

    Fix/Improve

--- sharedCode/src/commonTest/kotlin/de/joz/appcommander/helper/screenshot/ImageConverter.kt
+++ sharedCode/src/commonTest/kotlin/de/joz/appcommander/helper/screenshot/ImageConverter.kt
@@ -11,8 +11,18 @@ class ImageConverter {
 		Image.makeFromBitmap(bitmap).encodeToData(EncodedImageFormat.PNG, IMAGE_QUALITY)?.bytes
 
 	fun trim(bitmap: Bitmap): Bitmap {
-		val backgroundColor = bitmap.getColor(0, 0)
-		val bounds = computeTrimBounds(bitmap, backgroundColor)
+		val backgroundColorLeft = bitmap.getColor(0, 0)
+		val backgroundColorRight = bitmap.getColor(bitmap.width - 1, 0)
+		val backgroundColorTop = bitmap.getColor(0, 0)
+		val backgroundColorBottom = bitmap.getColor(0, bitmap.height - 1)
+
+		val bounds = computeTrimBounds(
+			bitmap = bitmap,
+			backgroundColorLeft = backgroundColorLeft,
+			backgroundColorRight = backgroundColorRight,
+			backgroundColorTop = backgroundColorTop,
+			backgroundColorBottom = backgroundColorBottom,
+		)
 
 		val w = abs(bounds.right - bounds.left) + 2 * PADDING
 		val h = abs(bounds.bottom - bounds.top) + 2 * PADDING
@@ -31,16 +41,19 @@ class ImageConverter {
 
 	private fun computeTrimBounds(
 		bitmap: Bitmap,
-		backgroundColor: Int,
+		backgroundColorLeft: Int,
+		backgroundColorRight: Int,
+		backgroundColorTop: Int,
+		backgroundColorBottom: Int,
 	): TrimBounds {
 		var trimBounds = TrimBounds()
 
 		for (x in 0 until bitmap.width) {
 			for (y in 0 until bitmap.height) {
-				if (bitmap.getColor(x, y) != backgroundColor && trimBounds.left == -1) {
+				if (bitmap.getColor(x, y) != backgroundColorLeft && trimBounds.left == -1) {
 					trimBounds = trimBounds.copy(left = x)
 				}
-				if (bitmap.getColor(bitmap.width - 1 - x, y) != backgroundColor && trimBounds.right == -1) {
+				if (bitmap.getColor(bitmap.width - 1 - x, y) != backgroundColorRight && trimBounds.right == -1) {
 					trimBounds = trimBounds.copy(right = bitmap.width - 1 - x)
 				}
 			}
@@ -48,10 +61,10 @@ class ImageConverter {
 
 		for (y in 0 until bitmap.height) {
 			for (x in 0 until bitmap.width) {
-				if (bitmap.getColor(x, y) != backgroundColor && trimBounds.top == -1) {
+				if (bitmap.getColor(x, y) != backgroundColorTop && trimBounds.top == -1) {
 					trimBounds = trimBounds.copy(top = y)
 				}
-				if (bitmap.getColor(x, bitmap.height - 1 - y) != backgroundColor && trimBounds.bottom == -1) {
+				if (bitmap.getColor(x, bitmap.height - 1 - y) != backgroundColorBottom && trimBounds.bottom == -1) {
 					trimBounds = trimBounds.copy(bottom = bitmap.height - 1 - y)
 				}
 			}
