8e137fbddf81de648b1cc9c260fb60ee5fb5a5fb
Jan Oliver Zieger <ollizieger@gmx.de>
2026-07-26 15:54:56 +0200

    Fix/Improve

--- sharedCode/src/commonTest/kotlin/de/joz/appcommander/helper/PreferencesRepositoryMock.kt
+++ sharedCode/src/commonTest/kotlin/de/joz/appcommander/helper/PreferencesRepositoryMock.kt
@@ -3,9 +3,10 @@ package de.joz.appcommander.helper
 import de.joz.appcommander.domain.preference.ChangedPreference
 import de.joz.appcommander.domain.preference.PreferencesRepository
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.MutableStateFlow
 
 class PreferencesRepositoryMock : PreferencesRepository {
+	private val prefsFlow = MutableStateFlow<List<ChangedPreference>>(emptyList())
 	var lastStoredValues = mutableMapOf<String, Any>()
 
 	override suspend fun get(
@@ -23,17 +24,13 @@ class PreferencesRepositoryMock : PreferencesRepository {
 		defaultValue: Int,
 	): Int = lastStoredValues[key] as? Int ?: defaultValue
 
-	override suspend fun getAsFlow(vararg keys: String): Flow<List<ChangedPreference>> =
-		flowOf(
-			lastStoredValues.map {
-				ChangedPreference(key = it.key, value = it.value)
-			},
-		)
+	override suspend fun getAsFlow(vararg keys: String): Flow<List<ChangedPreference>> = prefsFlow
 
 	override suspend fun <T> store(
 		key: String,
 		value: T,
 	) {
+		prefsFlow.value += ChangedPreference(key = key, value = value)
 		lastStoredValues[key] = value as Any
 	}
 }
