40199c7bb83e7267f48d536dfd54bcd91df2fe20
Jan Oliver Zieger <ollizieger@gmx.de>
2026-07-27 10:29:11 +0200

    Fix/Improve

--- sharedCode/src/commonTest/kotlin/de/joz/appcommander/e2e/EndToEndTest.kt
+++ sharedCode/src/commonTest/kotlin/de/joz/appcommander/e2e/EndToEndTest.kt
@@ -70,7 +70,7 @@ class EndToEndTest :
 		ScriptsRepository.Script(
 			label = "Dark mode test",
 			scripts = listOf("adb shell cmd uimode night yes TEST"),
-			platform = ScriptsRepository.Platform.ANDROID,
+			platform = ScriptsRepository.Platform.DESKTOP,
 		),
 		ScriptsRepository.Script(
 			label = "Toggle dark to light mode",
@@ -78,7 +78,7 @@ class EndToEndTest :
 				"adb shell cmd uimode night yes",
 				"adb shell cmd uimode night no",
 			),
-			platform = ScriptsRepository.Platform.ANDROID,
+			platform = ScriptsRepository.Platform.DESKTOP,
 		),
 		ScriptsRepository.Script(
 			label = "Light mode",
