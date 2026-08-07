f54ce94336b79115bf03d6b01e75d9ca54656e77
Jan Oliver Zieger <ollizieger@gmx.de>
2026-08-07 15:41:28 +0200

    Fix/Improve

--- sharedCode/src/commonMain/kotlin/de/joz/appcommander/ui/settings/SettingsScreen.kt
+++ sharedCode/src/commonMain/kotlin/de/joz/appcommander/ui/settings/SettingsScreen.kt
@@ -1,5 +1,6 @@
 package de.joz.appcommander.ui.settings
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -13,6 +14,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -91,7 +93,7 @@ internal fun SettingsContent(
 			Modifier
 				.fillMaxSize()
 				.padding(paddingValues)
-				.padding(16.dp)
+				.padding(horizontal = 16.dp)
 				.verticalScroll(rememberScrollState()),
 			verticalArrangement = Arrangement.spacedBy(16.dp),
 		) {
