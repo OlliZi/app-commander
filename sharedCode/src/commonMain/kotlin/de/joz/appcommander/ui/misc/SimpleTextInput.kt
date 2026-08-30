6b9d82796e3129c8da38aa9e8183acd1d0f2c806
Jan Oliver Zieger <ollizieger@gmx.de>
2026-08-30 10:55:21 +0200

    Fix/Improve

--- sharedCode/src/commonMain/kotlin/de/joz/appcommander/ui/misc/SimpleTextInput.kt
+++ sharedCode/src/commonMain/kotlin/de/joz/appcommander/ui/misc/SimpleTextInput.kt
@@ -1,6 +1,7 @@
 package de.joz.appcommander.ui.misc
 
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -16,6 +17,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import compose.icons.FeatherIcons
 import compose.icons.feathericons.X
 import de.joz.appcommander.ui.internalpreviews.DarkLightPreviewContainerProvider
@@ -30,6 +32,7 @@ fun SimpleTextInput(
 ) {
 	var inputValue by remember { mutableStateOf(value) }
 	TextField(
+		shape = RoundedCornerShape(10.dp),
 		value = inputValue,
 		modifier = modifier.fillMaxWidth().testTag(testTag),
 		colors = TextFieldDefaults.colors(
